e372e62ad3fbec38076c957959c7f6b89f3a33e4
bojo420 <drewandmalone@gmail.com>
2020-08-27 07:26:39 +0700

    Text edit

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import { Link } from "gatsby"
 const Home = () => (
   <div>
     <p>
-      Check out my <Link to="/about">about</Link>!
+      Check out my <Link to="/about">about page</Link>!
     </p>
   </div>
 )
